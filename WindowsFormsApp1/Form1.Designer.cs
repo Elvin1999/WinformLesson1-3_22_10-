01ac803f8514430a9fa36aab0d3744fe864cdf04
camalzadeelvin <camalzadeelvin@STHQ012D-01>
2023-10-03 11:14:51 +0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.timeLbl = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Comic Sans MS", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.titleLbl.ForeColor = System.Drawing.Color.OrangeRed;
+            this.titleLbl.Location = new System.Drawing.Point(94, 25);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(489, 67);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Happy Programmers";
+            // 
+            // timeLbl
+            // 
+            this.timeLbl.AutoSize = true;
+            this.timeLbl.Font = new System.Drawing.Font("Cooper Black", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLbl.ForeColor = System.Drawing.Color.Pink;
+            this.timeLbl.Location = new System.Drawing.Point(28, 150);
+            this.timeLbl.Name = "timeLbl";
+            this.timeLbl.Size = new System.Drawing.Size(1059, 110);
+            this.timeLbl.TabIndex = 0;
+            this.timeLbl.Text = "Happy Programmers";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(262, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Show Message";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SpringGreen;
+            this.ClientSize = new System.Drawing.Size(984, 576);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timeLbl);
+            this.Controls.Add(this.titleLbl);
+            this.MaximumSize = new System.Drawing.Size(1000, 800);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Label timeLbl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+         Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            titleLbl.Text = "Ilkin";
+            timeLbl.Text = DateTime.Now.ToLongTimeString();
+
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeLbl.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var result = MessageBox.Show("C# is hard or not ?","Lesson",MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            //if (result == DialogResult.Yes)
+            //{
+            //    this.BackColor = Color.Yellow;
+            //}
+            //else if(result == DialogResult.No)
+            //{
+            //    this.BackColor = Color.FromArgb(255, 0, 0);
+            //}
+
+            Button button = new Button();
+            button.Text = "Click Me";
+            button.AutoSize = true;
+            button.Font = new Font("Comic Sans MS", 25, FontStyle.Italic);
+            button.Location = new Point(100, 100);
+            button.ForeColor = Color.Brown;
+
+            button.Click += Button_Click;
+
+            this.Controls.Add(button);
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+           if(sender is Button bt)
+            {
+                this.Controls.Remove(bt);
+
+                Button button = new Button();
+                button.Text = "Click Me";
+                button.AutoSize = true;
+                button.Font = new Font("Comic Sans MS", 25, FontStyle.Italic);
+                var random=new Random();
+                var x = random.Next(100, 600);
+                var y = random.Next(100, 500);
+                button.Location = new Point(x, y);
+                button.ForeColor = Color.Brown;
+                button.Click += Button_Click;
+
+                this.Controls.Add(button);
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        //private void timeLbl_Click(object sender, EventArgs e)
+        //{
+        //    timer.Stop();
+        //}
+    }
+}
